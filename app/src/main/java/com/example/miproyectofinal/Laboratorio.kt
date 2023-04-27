f7ff1ddc8742ef00896452442a3259081e25d7b8
Alcides4002 <carlos.mendoza13222@est.uca.edu.ni>
2023-04-27 15:07:12 -0600

    segundo commit

---
+++ app/src/main/java/com/example/miproyectofinal/Laboratorio.kt
@@ -0,0 +1,20 @@
+package com.example.miproyectofinal
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "laboratorio")
+ data class Laboratorio (
+
+        @ColumnInfo(name = "nombre") var nombre:String,
+        @ColumnInfo(name = "Descripcion") var  descripcion:String,
+        @ColumnInfo(name = "horarios") var horarios:String,
+        @PrimaryKey(autoGenerate = true)
+        val id:Int = 0
+
+
+
+    )
+
---
+++ app/src/main/java/com/example/miproyectofinal/dbLaboratorio.kt
@@ -0,0 +1,13 @@
+package com.example.miproyectofinal
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Laboratorio::class],
+    version = 1
+)
+abstract class  dbLaboratorio RoomDataBase() {
+abstract fun daoLaboratorio():DaoLaboratorio
+}
+
